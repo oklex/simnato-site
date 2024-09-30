3fc1dfeb205c739a95fbc2333e3549e9c6074db0
Alexander Kim <alexkim.347@gmail.com>
2024-09-30 00:24:32 -0700

    added gold logo

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,37 @@
-import React from 'react';
-import Section from './ui-library/Section';
-import Header from './ui-library/Header';
-import Text from './ui-library/Text';
-import StyledText from './ui-library/StyledText';
+import React from "react";
+import Section from "./ui-library/Section";
+import Header from "./ui-library/Header";
+import Text from "./ui-library/Text";
+import StyledText from "./ui-library/StyledText";
+import { GoldBrandLogo } from "./assets/goldLogo";
 
 // Define the App component with React.FC
 const App: React.FC = () => (
-	<div id="app">
-		<Section mode="light" gradient="vertical">
-			<Header level={1} mode="dark">
-				Light Mode with <StyledText bold>Vertical Gradient</StyledText>
-			</Header>
-			<Text>This section has a vertical gradient background.</Text>
-		</Section>
+  <div id="app">
+    <Section mode="light" gradient="horizontal">
+      <Header level={1} mode="dark">
+        Light Mode with <StyledText bold>Horizontal Gradient</StyledText>
+      </Header>
+      <Text>This section has a vertical gradient background.</Text>
+    </Section>
 
-		<Section mode="dark" gradient="horizontal">
-			<Header level={1} mode="light">
-				Dark Mode with <StyledText bold>Horizontal Gradient</StyledText>
-			</Header>
-			<Text mode="light">
-				This section has a horizontal gradient background.
-			</Text>
-		</Section>
+    <Section mode="dark" gradient="vertical">
+      <GoldBrandLogo scale="300px" center padding="150px 0px 50px" />
+      <Header level={1} mode="light">
+        Dark Mode with <StyledText bold>Vertical Gradient</StyledText>
+      </Header>
+      <Text mode="light">
+        This section has a horizontal gradient background.
+      </Text>
+    </Section>
 
-		<Section mode="gold">
-			<Header level={1} mode="dark">
-				Gold Mode <StyledText bold>without Gradient</StyledText>
-			</Header>
-			<Text>This section has a solid background color.</Text>
-		</Section>
-	</div>
+    <Section mode="gold">
+      <Header level={1} mode="dark">
+        Gold Mode <StyledText bold>without Gradient</StyledText>
+      </Header>
+      <Text>This section has a solid background color.</Text>
+    </Section>
+  </div>
 );
 
 export default App;
