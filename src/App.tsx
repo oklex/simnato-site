d40a2a532756c5848630f736553c6c1c2b3f9995
Alexander Kim <alexkim.347@gmail.com>
2024-09-30 19:58:43 -0700

    updated main page

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,4 @@
 import React from "react";
-import Section from "./ui-library/Section";
-import Header from "./ui-library/Header";
-import Text from "./ui-library/Text";
-import StyledText from "./ui-library/StyledText";
 import GoldBrandLogo from "./assets/goldLogo";
 import TextLogo from "./assets/textLogo";
 import { HomepageHero } from "./components/homepageHero";
@@ -10,31 +6,7 @@ import { HomepageHero } from "./components/homepageHero";
 // Define the App component with React.FC
 const App: React.FC = () => (
   <div id="app">
-	<HomepageHero/>
-    {/* <Section mode="light" gradient="horizontal">
-      <TextLogo height={"35px"} />
-      <Header level={1} mode="dark">
-        Light Mode with <StyledText bold>Horizontal Gradient</StyledText>
-      </Header>
-      <Text>This section has a vertical gradient background.</Text>
-    </Section>
-
-    <Section mode="dark" gradient="vertical">
-      <GoldBrandLogo scale="300px" center padding="150px 0px 50px" />
-      <Header level={1} mode="light">
-        Dark Mode with <StyledText bold>Vertical Gradient</StyledText>
-      </Header>
-      <Text mode="light">
-        This section has a horizontal gradient background.
-      </Text>
-    </Section>
-
-    <Section mode="gold">
-      <Header level={1} mode="dark">
-        Gold Mode <StyledText bold>without Gradient</StyledText>
-      </Header>
-      <Text>This section has a solid background color.</Text>
-    </Section> */}
+    <HomepageHero />
   </div>
 );
 
---
+++ src/components/registrationInfoSlice.tsx
@@ -0,0 +1,85 @@
+import React, { ReactElement } from "react";
+import styled from "styled-components";
+import { StyledText, Text } from "../ui-library";
+
+export const RegistrationInfoSlice = (): ReactElement => {
+  return (
+    <div id="registration-info">
+      <OuterContainer>
+        <InnerContainer>
+          <Cell align="right" widthPercentage={20}>
+            <Text mode="light">club registration</Text>
+          </Cell>
+          <Cell align="left" padding="0 0 0 10%">
+            free
+          </Cell>
+          <Cell widthPercentage={20}></Cell>
+        </InnerContainer>
+
+        <InnerContainer>
+          <Cell align="right" widthPercentage={20}>
+            <Text mode="light">priority registration</Text>
+            <Text mode="light" size="sm" subtle>
+              Oct. 29 - Dec 1st
+            </Text>
+          </Cell>
+          <Cell align="left" padding="0 0 0 10%">
+            $125{"  "}
+            <StyledText subtle size="sm" horizontalPadding="0.5rem">
+              (overnight $230)
+            </StyledText>
+          </Cell>
+          <Cell widthPercentage={20}>see more</Cell>
+        </InnerContainer>
+
+        <InnerContainer>
+          <Cell align="right" widthPercentage={20}>
+            <Text mode="light">late registration</Text>
+            <Text mode="light" size="sm" subtle>
+              Dec. 2nd - tba
+            </Text>
+          </Cell>
+          <Cell align="left" padding="0 0 0 10%">
+            tba
+            <StyledText subtle size="sm" horizontalPadding="0.5rem">
+              (overnight tba)
+            </StyledText>
+          </Cell>
+          <Cell widthPercentage={20}>see more</Cell>
+        </InnerContainer>
+      </OuterContainer>
+    </div>
+  );
+};
+
+const OuterContainer = styled.div`
+  margin-top: 2rem;
+  margin-left: auto;
+  margin-right: auto;
+  display: flex;
+  flex-direction: column;
+`;
+
+const InnerContainer = styled.div`
+  display: flex;
+  justify-content: center; /* Centers items horizontally */
+  flex-grow: 1;
+  gap: 1rem;
+`;
+
+const Cell = styled.div<{
+  widthPercentage?: number;
+  align?: "left" | "center" | "right";
+  padding?: string;
+}>`
+  flex-grow: 1;
+  width: ${({ widthPercentage }) => widthPercentage ?? 30}%;
+
+  & > * {
+    text-align: ${({ align }) => align || "center"}; /* Text alignment */
+    align-items: center; /* Vertically center content */
+    align-self: center;
+  }
+  padding: ${({ padding }) => padding ?? "0.25rem"};
+  box-sizing: border-box;
+`;
