c32de16a1c8e8a07223f18f9b7fc04a1bf8218fc
Alexander Kim <alexkim.347@gmail.com>
2024-09-30 17:54:35 -0700

    updated page content

--- src/components/RegistrationNav.tsx
+++ src/components/RegistrationNav.tsx
@@ -1,71 +1,76 @@
-import React, { ReactElement } from 'react';
-import styled from 'styled-components';
-import { rgba } from 'polished';
-import { PALETTE } from '../theme';
+import React, { ReactElement } from "react";
+import styled from "styled-components";
+import { rgba } from "polished";
+import { PALETTE } from "../theme";
+import TextLogo from "../assets/textLogo";
+import { Text } from "../ui-library";
 
 export const RegistrationNav = (): ReactElement => {
-	return (
-		<NavigationWrapper id="registration-nav">
-			<StyledNav>
-				<NavContent>
-					<span>logo</span>
-					<span>register</span>
-				</NavContent>
-			</StyledNav>
-		</NavigationWrapper>
-	);
+  return (
+    <NavigationWrapper id="registration-nav">
+      <StyledNav>
+        <NavContent>
+          <TextLogo height="1.5rem" />
+          <Text mode="gold" bold uppercase spaced>
+            register
+          </Text>
+        </NavContent>
+      </StyledNav>
+    </NavigationWrapper>
+  );
 };
 
 const NavContent = styled.div`
-	width: 100%;
-	display: flex;
-	justify-content: space-between;
+  width: 100%;
+  display: flex;
+  justify-content: space-between;
 `;
 
 const NavigationWrapper = styled.div`
-	display: flex;
-	justify-content: center;
-	// I think this should work
-	position: sticky;
-	top: 25px;
-	// matches container
-	max-width: 1200px;
-	padding: 20px 20px;
+  display: flex;
+  justify-content: center;
+  // I think this should work
+  position: sticky;
+  top: 25px;
+  // matches container
+  max-width: 1200px;
+  padding: 20px 20px;
 `;
 
 const StyledNav = styled.button`
-	padding: 1rem 1.3rem;
-	width: 100%;
+  padding: 0.8rem 1rem;
+  width: 100%;
 
-	border: none;
-	outline: none;
-	position: relative;
-	z-index: 1;
-	border-radius: 20px;
-	background: linear-gradient(
-		to right,
-		${PALETTE.gold.main},
-		${rgba(PALETTE.gold.light, 0.1)}
-	);
-	transition: 200ms;
+  border: none;
+  outline: none;
+  position: relative;
+  z-index: 1;
+  border-radius: 20px;
+  background: linear-gradient(
+    to right,
+    ${PALETTE.gold.main},
+    ${rgba(PALETTE.gold.light, 0.1)}
+  );
+  transition: 200ms;
+  align-items: center;
 
-	cursor: pointer;
-	overflow: hidden;
+  cursor: pointer;
+  overflow: hidden;
 
-	&::before {
-		content: '';
-		position: absolute;
-		left: 1px;
-		right: 1px;
-		top: 1px;
-		bottom: 1px;
-		border-radius: 20px;
-		background-color: #313979;
-		z-index: -1;
-		transition: 400ms;
-	}
+  &::before {
+    content: "";
+    position: absolute;
+    left: 1px;
+    right: 1px;
+    top: 1px;
+    bottom: 1px;
+    border-radius: 20px;
+    background-color: #313979;
+    z-index: -1;
+    transition: 400ms;
+  }
 
-	&:hover::before {
-		opacity: 80%;
-	}
+  &:hover::before {
+    opacity: 80%;
+  }
 `;
--- src/components/homepageHero.tsx
+++ src/components/homepageHero.tsx
@@ -1,26 +1,45 @@
-import React, { ReactElement } from 'react';
-import Section from '../ui-library/Section';
-import GoldBrandLogo from '../assets/goldLogo';
-import Header from '../ui-library/Header';
-import Spacer from '../ui-library/Spacer';
-import { RegistrationNav } from './RegistrationNav';
+import React, { ReactElement } from "react";
+import GoldBrandLogo from "../assets/goldLogo";
+import { Header, Spacer, Text, Section } from "../ui-library";
+import { RegistrationNav } from "./RegistrationNav";
+import styled from "styled-components";
+import { PALETTE } from "../theme";
 
 export const HomepageHero = (): ReactElement => {
-	return (
-		<div id="homepage-hero">
-			<Section center height="90vh" mode="dark" gradient="vertical">
-				<Spacer height={'30vh'} />
-				<GoldBrandLogo scale="18rem" center padding="25px 25px 25px" />
-				<Header level={1} sizeRemOverride={6.3} mode="gold" center bold>
-					Simulation NATO
-				</Header>
-				<Header margin='-15px 0 auto' level={6} sizeRemOverride={2.3} mode="light" center>
-					Model UN Crisis Conference
-				</Header>
-				<RegistrationNav/>
-			</Section>
-		</div>
-	);
+  return (
+    <div id="homepage-hero" style={{ position: "relative" }}>
+      <CornerLabel>
+        <Text>EST. 2024</Text>
+      </CornerLabel>
+      <Section center height="90vh" mode="dark" gradient="vertical">
+        <Spacer height={"30vh"} />
+        <GoldBrandLogo scale="18rem" center padding="25px 25px 25px" />
+        <Header level={1} sizeRemOverride={6.3} mode="gold" center bold>
+          Simulation NATO
+        </Header>
+        <Header
+          margin="-15px 0 auto"
+          level={6}
+          sizeRemOverride={2.3}
+          mode="light"
+          center
+        >
+          Model UN Crisis Conference
+        </Header>
+        <RegistrationNav />
+      </Section>
+    </div>
+  );
 };
 
+const CornerLabel = styled.div`
+  position: absolute;
+  top: 30px;
+  left: 30px;
 
+  & > * {
+    color: ${PALETTE.blue.light};
+    letter-spacing: 1.46em;
+    font-size: 0.6rem;
+  }
+`;
