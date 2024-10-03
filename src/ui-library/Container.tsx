7177421b8108785a8ca09a1a2e3e1eb13dac172c
Alexander Kim <alexander@refractionpoint.com>
2024-10-03 15:39:14 -0700

    updated container prop with max width

--- src/ui-library/Container.tsx
+++ src/ui-library/Container.tsx
@@ -1,17 +1,17 @@
-import styled, { css } from "styled-components";
+import styled, { css } from 'styled-components';
 
 // Generic responsive container inside the full-width section
-export const Container = styled.div<{ center?: boolean }>`
-  max-width: 1200px;
-  padding-left: 1rem;
-  padding-right: 1rem;
-  margin-left: auto;
-  margin-right: auto;
-  ${({ center }) =>
-    center &&
-    css`
-      display: flex;
-      flex-direction: column;
-      justify-content: center;
-    `}
+export const Container = styled.div<{ center?: boolean; maxWidth?: number }>`
+	max-width: ${({ maxWidth }) => `${maxWidth ?? 1200}px`};
+	padding-left: 1rem;
+	padding-right: 1rem;
+	margin-left: auto;
+	margin-right: auto;
+	${({ center }) =>
+		center &&
+		css`
+			display: flex;
+			flex-direction: column;
+			justify-content: center;
+		`}
 `;
