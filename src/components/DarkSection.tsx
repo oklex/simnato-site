8916877c9da040500e4b2dce04cb443599ecdffd
Alexander Kim <alexander@refractionpoint.com>
2024-10-26 15:41:19 -0700

    adding track

--- src/components/DarkSection.tsx
+++ src/components/DarkSection.tsx
@@ -1,8 +1,17 @@
-import { ReactElement, ReactNode, Ref, useContext } from 'react';
+import {
+	ReactElement,
+	ReactNode,
+	Ref,
+	useContext,
+	useEffect,
+	useRef,
+	useState,
+} from 'react';
 import styled from 'styled-components';
 
-import { NavThemeContext } from '@src/context/navTheme';
-import { Section } from '@src/ui-library';
+import { NavThemeContext } from '@context/navTheme';
+import { Section } from '@ui-library';
+import { useScrollPosition } from '@stores/useScrollPosition';
 
 type DarkSectionType = {
 	id: string;
@@ -14,21 +23,35 @@ export const DarkSection = ({
 	children,
 }: DarkSectionType): ReactElement => {
 	const { initializeRef } = useContext(NavThemeContext);
+	const ref = useRef<HTMLDivElement | null>(null);
+	const { scrollY } = useScrollPosition();
+	const [scrollOffset, setScrollOffset] = useState<number | null>(null);
+
 	const refCallback = (instance: HTMLDivElement | null) => {
-		if (instance) initializeRef(id, { current: instance });
+		if (instance) {
+			initializeRef(id, { current: instance });
+			ref.current = instance;
+		}
 	};
 
+	useEffect(() => {
+		if (ref.current) setScrollOffset(ref.current.getBoundingClientRect().top);
+	}, [ref, scrollY]);
+
 	return (
-		<div ref={refCallback}>
-			<Section mode="dark" gradient={'vertical'}>
+		<TrackWrapper ref={refCallback}>
+			<MaskWrapper mode="dark" gradient={'vertical'}>
 				{children}
-			</Section>
-		</div>
+			</MaskWrapper>
+		</TrackWrapper>
 	);
 };
 
-const MaskWrapper = styled.div`
-	position: fixed;
+// the track contains the full height of the content
+const TrackWrapper = styled.div``;
+
+const MaskWrapper = styled(Section)`
+	position: sticky;
 	top: 0px;
 	left: 0px;
 
@@ -36,4 +59,8 @@ const MaskWrapper = styled.div`
 	height: 100vh;
 	padding: 0.8rem 0.9rem;
 	pointer-events: none;
+`;
+
+const Mask = styled.div<{ '-invHeight': number }>`
+	overflow-y: hidden;
 `;
