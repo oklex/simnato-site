360b5e23b52af5db7461205c4452d3f0f6681062
Alexander Kim <alexkim.347@gmail.com>
2024-10-26 16:13:18 -0700

    updated styling

--- src/components/DarkSection.tsx
+++ src/components/DarkSection.tsx
@@ -1,66 +1,91 @@
 import {
-	ReactElement,
-	ReactNode,
-	Ref,
-	useContext,
-	useEffect,
-	useRef,
-	useState,
-} from 'react';
-import styled from 'styled-components';
+  ReactElement,
+  ReactNode,
+  Ref,
+  useContext,
+  useEffect,
+  useRef,
+  useState,
+} from "react";
+import styled from "styled-components";
 
-import { NavThemeContext } from '@context/navTheme';
-import { Section } from '@ui-library';
-import { useScrollPosition } from '@stores/useScrollPosition';
+import { NavThemeContext } from "@context/navTheme";
+import { useScrollPosition } from "@stores/useScrollPosition";
+import { GRADIENTS } from "@src/theme";
 
 type DarkSectionType = {
-	id: string;
-	children: ReactNode | ReactNode[] | null;
+  id: string;
+  children: ReactNode | ReactNode[] | null;
 };
 
 export const DarkSection = ({
-	id,
-	children,
+  id,
+  children,
 }: DarkSectionType): ReactElement => {
-	const { initializeRef } = useContext(NavThemeContext);
-	const ref = useRef<HTMLDivElement | null>(null);
-	const { scrollY } = useScrollPosition();
-	const [scrollOffset, setScrollOffset] = useState<number | null>(null);
+  const { initializeRef } = useContext(NavThemeContext);
+  const ref = useRef<HTMLDivElement | null>(null);
+  const { scrollY } = useScrollPosition();
+  const [scrollOffset, setScrollOffset] = useState<number | null>(null);
 
-	const refCallback = (instance: HTMLDivElement | null) => {
-		if (instance) {
-			initializeRef(id, { current: instance });
-			ref.current = instance;
-		}
-	};
+  const refCallback = (instance: HTMLDivElement | null) => {
+    if (instance) {
+      initializeRef(id, { current: instance });
+      ref.current = instance;
+    }
+  };
 
-	useEffect(() => {
-		if (ref.current) setScrollOffset(ref.current.getBoundingClientRect().top);
-	}, [ref, scrollY]);
+  useEffect(() => {
+    if (ref.current) setScrollOffset(ref.current.getBoundingClientRect().top);
+    console.log(
+      id,
+      "dark section context",
+      {
+        innerHeight: window.innerHeight,
+        scrollY: window.scrollY,
+        trackTop: ref.current.getBoundingClientRect().top,
+      },
+      {
+        inverseTop:
+          ref.current.getBoundingClientRect().top > 0
+            ? ref.current.getBoundingClientRect().top
+            : 0,
+        // inverseBot: window.scrollY
+      }
+    );
+  }, [ref, scrollY]);
 
-	return (
-		<TrackWrapper ref={refCallback}>
-			<MaskWrapper mode="dark" gradient={'vertical'}>
-				{children}
-			</MaskWrapper>
-		</TrackWrapper>
-	);
+  return (
+    <TrackWrapper id="track-wrapper" ref={refCallback}>
+      <MaskWrapper>
+        <CustomSection>{children}</CustomSection>
+      </MaskWrapper>
+    </TrackWrapper>
+  );
 };
 
 // the track contains the full height of the content
-const TrackWrapper = styled.div``;
-
-const MaskWrapper = styled(Section)`
-	position: sticky;
-	top: 0px;
-	left: 0px;
-
-	width: 100%;
-	height: 100vh;
-	padding: 0.8rem 0.9rem;
-	pointer-events: none;
+const TrackWrapper = styled.div`
+  display: relative;
 `;
 
-const Mask = styled.div<{ '-invHeight': number }>`
-	overflow-y: hidden;
+const MaskWrapper = styled.div`
+  position: sticky;
+  top: 0px;
+  left: 0px;
+
+  max-height: calc(100vh - 1.6rem);
+  pointer-events: none;
+  overflow-y: hidden;
+  border-radius: 20px;
+`;
+
+const CustomSection = styled.div`
+  padding: 0.8rem 0.9rem;
+  border-radius: 20px;
+
+  background: linear-gradient(to bottom, ${GRADIENTS.main});
+`;
+
+const Mask = styled.div<{ "-invHeight": number }>`
+  overflow-y: hidden;
 `;
--- src/pages/section-test.tsx
+++ src/pages/section-test.tsx
@@ -1,26 +1,27 @@
-import { DarkSection } from '@src/components/DarkSection';
-import { ReactElement } from 'react';
-import styled from 'styled-components';
+import { DarkSection } from "@src/components/DarkSection";
+import { ReactElement } from "react";
+import styled from "styled-components";
 
 const SectionTestPage = (): ReactElement => {
-	return (
-		<div id="section-test-page">
-			<DarkSection id={'hero'}>
+  return (
+    <div id="section-test-page">
+      <DarkSection id={"hero"}>
+        <Filler></Filler>
+      </DarkSection>
+      <Filler></Filler>
+      <DarkSection id={"part 2"}>
+        <Filler size={350}></Filler>
+      </DarkSection>
+      {/* <DarkSection id={'part 3'}>
 				<Filler></Filler>
-			</DarkSection>
-			<Filler></Filler>
-			<DarkSection id={'part 2'}>
-				<Filler size={350}></Filler>
-			</DarkSection>
-			<DarkSection id={'part 3'}>
-				<Filler></Filler>
-			</DarkSection>
-		</div>
-	);
+			</DarkSection> */}
+      <Filler size={350}></Filler>
+    </div>
+  );
 };
 
 export default SectionTestPage;
 
-const Filler = styled.div<{size?: number}>`
-	min-height: ${({ size }) => `${size ?? 30}vh` };
+const Filler = styled.div<{ size?: number }>`
+  min-height: ${({ size }) => `${size ?? 30}vh`};
 `;
