f259e75a1949d7ac0bc2762776ebe146b45bad08
Alexander Kim <alexkim.347@gmail.com>
2024-10-25 22:28:35 -0700

    added a prototype component for darksection

---
+++ src/components/DarkSection.tsx
@@ -0,0 +1,32 @@
+import { ReactElement, ReactNode, Ref, useContext } from "react";
+import styled from "styled-components";
+
+import { NavThemeContext } from "@src/context/navTheme";
+
+type DarkSectionType = {
+  id: string;
+  children: ReactNode | ReactNode[] | null;
+};
+
+export const DarkSection = ({
+  id,
+  children,
+}: DarkSectionType): ReactElement => {
+  const { initializeRef } = useContext(NavThemeContext);
+  const refCallback = (instance: HTMLDivElement | null) => {
+    if (instance) initializeRef(id, { current: instance });
+  };
+
+  return <div ref={refCallback}>{children}</div>;
+};
+
+const MaskWrapper = styled.div`
+  position: fixed;
+  top: 0px;
+  left: 0px;
+
+  width: 100%;
+  height: 100vh;
+  padding: 0.8rem 0.9rem;
+  pointer-events: none;
+`;
