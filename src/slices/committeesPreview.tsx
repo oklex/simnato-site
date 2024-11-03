555a7d06b504069e5dd0177a0fca8c01979c25b9
Alexander Kim <alexkim.347@gmail.com>
2024-11-03 00:41:43 -0700

    updated committee preview slice

--- src/slices/committeesPreview.tsx
+++ src/slices/committeesPreview.tsx
@@ -74,16 +74,7 @@ export const CommitteesPreview = (): ReactElement => {
         </Container>
         <Spacer height={"200px"} />
         <Container>
-          {/* <Text>lorem ipsum</Text>
-          <Text>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
-            ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-            aliquip ex ea commodo consequat. Duis aute irure dolor in
-            reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
-            pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
-            culpa qui officia deserunt mollit anim id est laborum.
-          </Text> */}
+          <Text>Register Now</Text>
         </Container>
       </SubContent>
     </DarkSection>
