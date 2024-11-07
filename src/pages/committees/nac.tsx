4372e6c776f71099b28b4a477a3f6a3524e1bd8b
Alexander Kim <alexander@refractionpoint.com>
2024-11-07 17:53:42 -0500

    added prototype committee page

--- src/pages/committees/nac.tsx
+++ src/pages/committees/nac.tsx
@@ -2,48 +2,26 @@ import { ReactElement } from 'react';
 import styled from 'styled-components';
 
 import { ViewPortSection } from '@components/ViewPortSection';
-import { Container, Header, Spacer, Text } from '@ui-library';
+import { Container, Header, NarrowContainer, Spacer, Text } from '@ui-library';
 
 export const NACpage = (): ReactElement => {
 	return (
 		<div>
-			<Container>
-				<ViewPortSection id="header">
-					<Spacer height={'20vh'} />
-					<Header level={1}>North Atlantic Council</Header>
-					<Spacer height={'20vh'} />
+			<NarrowContainer>
+				<Spacer height={'20vh'} />
+				<Header center level={1} mode="dark">
+					North Atlantic Council
+				</Header>
+				<Spacer height={'20vh'} />
+			</NarrowContainer>
+			<Spacer />
+			<NarrowContainer>
+				<ViewPortSection id="info-box" mode="mono">
+					<Text mode="dark">overview</Text>
 				</ViewPortSection>
-				<SplitScreen>
-					<SectionWrapper>
-						<ViewPortSection id="left" mode='light'>
-							<Spacer height={'30vh'} />
-							<Text>text</Text>
-							<Spacer height={'30vh'} />
-						</ViewPortSection>
-					</SectionWrapper>
-
-					<SectionWrapper>
-						<ViewPortSection id="right" mode='light'>
-							<Spacer height={'30vh'} />
-							<Text>text</Text>
-							<Spacer height={'30vh'} />
-						</ViewPortSection>
-					</SectionWrapper>
-				</SplitScreen>
-			</Container>
+			</NarrowContainer>
 		</div>
 	);
 };
 
 export default NACpage;
-
-const SplitScreen = styled.div`
-	display: flex;
-	gap: 15px;
-	width: 100%;
-	justify-content: space-between;
-`;
-
-const SectionWrapper = styled.div`
-	flex: 1;
-`;
