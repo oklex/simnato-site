a026f38921e97aab1638087e818e5d08c31167c9
Alexander Kim <alexkim.347@gmail.com>
2024-11-13 19:21:46 -0800

    custom layout for mc page

--- src/pages/committees/mc.tsx
+++ src/pages/committees/mc.tsx
@@ -29,44 +29,42 @@ export const MCpage = (): ReactElement => {
         <Spacer />
       </NarrowContainer>
       <Spacer />
-      <NarrowContainer>
-        <LetterGrid>
-          <Letter>
-            <Text mode="dark" align="center">
-              <StyledText bold>-- URGENT READ IMMEDIATELY --</StyledText>
-            </Text>
-            <Spacer />
-            <Text mode="dark">To all NATO Military Commanders: </Text>
-            <Text mode="dark">
-              Ukraine must not fall into Russian hands. It will be the fracture
-              that will split NATO apart, we must unite as one and curb Putin’s
-              aggression once and for all. We have prepared a simulation for you
-              this March, we expect your undivided attention and presence. This
-              will determine the future of this council, and its ability to stop
-              other countries’ aggressive behaviour. Failure to do so puts the
-              council, and the global community, in a devastating position. The
-              fall of Ukraine will be the first of many in our various partners
-              around the world, it must not happen at any costs. We hope that we
-              chose the right individuals for this job, the world is counting on
-              us.
-            </Text>
-            <Text mode="dark">Godspeed. </Text>
-          </Letter>
+      <Letter>
+        <Header level={5} mode="dark" center>
+          <StyledText bold>-- URGENT READ IMMEDIATELY --</StyledText>
+        </Header>
+        <Spacer />
+        <Spacer />
+        <Text mode="dark">To all NATO Military Commanders: </Text>
+        <Text mode="dark">
+          Ukraine must not fall into Russian hands. It will be the fracture that
+          will split NATO apart, we must unite as one and curb Putin’s
+          aggression once and for all. We have prepared a simulation for you
+          this March, we expect your undivided attention and presence. This will
+          determine the future of this council, and its ability to stop other
+          countries’ aggressive behaviour. Failure to do so puts the council,
+          and the global community, in a devastating position. The fall of
+          Ukraine will be the first of many in our various partners around the
+          world, it must not happen at any costs. We hope that we chose the
+          right individuals for this job, the world is counting on us.
+        </Text>
+        <Text mode="dark">Godspeed. </Text>
 
-          <Div flex flexDirection="col" gap="15px" padding="10px">
-            <Portrait
-              fullName="Marek Cai"
-              position="Co-Director"
-              width="medium"
-            />
-            <Portrait
-              fullName="Mary Lu"
-              position="Co-Director"
-              width="medium"
-            />
-          </Div>
-        </LetterGrid>
-      </NarrowContainer>
+        <Div
+          flex
+          flexDirection="row"
+          justifyContent="center"
+          gap="15px"
+          padding="10px"
+        >
+          <Portrait
+            fullName="Marek Cai"
+            position="Co-Director"
+            width="medium"
+          />
+          <Portrait fullName="Mary Lu" position="Co-Director" width="medium" />
+        </Div>
+      </Letter>
     </div>
   );
 };
@@ -86,6 +84,7 @@ const LetterGrid = styled.div`
   }
 `;
 
-const Letter = styled.div`
+const Letter = styled(NarrowContainer)`
   grid-column: span 2;
+  max-width: 720px;
 `;
