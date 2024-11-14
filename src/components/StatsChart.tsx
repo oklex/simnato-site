f6dd61a9d35ad7b5485376e3799920f8d222862a
Alexander Kim <alexander@refractionpoint.com>
2024-11-14 13:01:27 -0800

    reformatting stats chart file

--- src/components/StatsChart.tsx
+++ src/components/StatsChart.tsx
@@ -1,10 +1,10 @@
-import { createRef, ReactElement } from "react";
-import styled, { css } from "styled-components";
+import { createRef, ReactElement } from 'react';
+import styled, { css } from 'styled-components';
 
-import { useHover } from "@stores/useHover";
-import useScreenSize from "@stores/useScreenSize";
-import { BREAKPOINTS } from "@src/theme";
-import { Div, Text } from "@ui-library";
+import { useHover } from '@stores/useHover';
+import useScreenSize from '@stores/useScreenSize';
+import { BREAKPOINTS } from '@src/theme';
+import { Div, Text } from '@ui-library';
 
 type Score = 1 | 2 | 3 | 4 | 5;
 type Coordinate = string;
@@ -14,349 +14,353 @@ const VIEWBOX_PADDING = 15;
 const VIEWBOX_DIMENSION = BASE_VIEWBOX_SIZE + VIEWBOX_PADDING * 2;
 
 export type CommitteeStatsTypes = {
-  knowledge: Score;
-  speeches: Score;
-  pacing: Score;
-  moderation: Score;
+	knowledge: Score;
+	speeches: Score;
+	pacing: Score;
+	moderation: Score;
 };
 interface StatsProps {
-  committeeStats: CommitteeStatsTypes;
+	committeeStats: CommitteeStatsTypes;
 }
 
 const statDescription = {
-  knowledge: {
-    "1": "little research is required",
-    "2": "some research is required",
-    "3": "a regular amount of research is recommended",
-    "4": "extra research is recommended",
-    "5": "lots of research is recommended",
-  },
-  speeches: {
-    "1": "expect mostly smaller conversations",
-    "2": "expect a mix of smaller conversations and speeches",
-    "3": "speeches may be impactful but aren't essential",
-    "4": "expect speeches to be important",
-    "5": "expect speeches to be essential",
-  },
-  pacing: {
-    "1": "expected to be slow paced",
-    "2": "expected to be slower at times",
-    "3": "can vary widely from slow to fast",
-    "4": "expected to be faster at times",
-    "5": "expected to be fast paced",
-  },
-  moderation: {
-    "1": "almost entirely unmoderated",
-    "2": "mostly unmoderated, some moderated",
-    "3": "mixed moderated and unmoderated",
-    "4": "mostly moderated debate, some unmod",
-    "5": "almost entirely moderated",
-  },
+	knowledge: {
+		'1': 'little research is required',
+		'2': 'some research is required',
+		'3': 'a regular amount of research is recommended',
+		'4': 'extra research is recommended',
+		'5': 'lots of research is recommended',
+	},
+	speeches: {
+		'1': 'expect mostly smaller conversations',
+		'2': 'expect a mix of smaller conversations and speeches',
+		'3': "speeches may be impactful but aren't essential",
+		'4': 'expect speeches to be important',
+		'5': 'expect speeches to be essential',
+	},
+	pacing: {
+		'1': 'expected to be slow paced',
+		'2': 'expected to be slower at times',
+		'3': 'can vary widely from slow to fast',
+		'4': 'expected to be faster at times',
+		'5': 'expected to be fast paced',
+	},
+	moderation: {
+		'1': 'almost entirely unmoderated',
+		'2': 'mostly unmoderated, some moderated',
+		'3': 'mixed moderated and unmoderated',
+		'4': 'mostly moderated debate, some unmod',
+		'5': 'almost entirely moderated',
+	},
 };
 
 export const StatsChart = ({ committeeStats }: StatsProps): ReactElement => {
-  const { knowledge, speeches, pacing, moderation } = committeeStats;
-  const { isMobile } = useScreenSize();
+	const { knowledge, speeches, pacing, moderation } = committeeStats;
+	const { isMobile } = useScreenSize();
 
-  const ref1_top = createRef<SVGCircleElement>();
-  const ref2_right = createRef<SVGCircleElement>();
-  const ref3_bottom = createRef<SVGCircleElement>();
-  const ref4_left = createRef<SVGCircleElement>();
+	const ref1_top = createRef<SVGCircleElement>();
+	const ref2_right = createRef<SVGCircleElement>();
+	const ref3_bottom = createRef<SVGCircleElement>();
+	const ref4_left = createRef<SVGCircleElement>();
 
-  const ref1_hover = useHover(ref1_top);
-  const ref2_hover = useHover(ref2_right);
-  const ref3_hover = useHover(ref3_bottom);
-  const ref4_hover = useHover(ref4_left);
+	const ref1_hover = useHover(ref1_top);
+	const ref2_hover = useHover(ref2_right);
+	const ref3_hover = useHover(ref3_bottom);
+	const ref4_hover = useHover(ref4_left);
 
-  const ref1_text = statDescription.knowledge[knowledge];
-  const ref2_text = statDescription.speeches[speeches];
-  const ref3_text = statDescription.pacing[pacing];
-  const ref4_text = statDescription.moderation[moderation];
+	const ref1_text = statDescription.knowledge[knowledge];
+	const ref2_text = statDescription.speeches[speeches];
+	const ref3_text = statDescription.pacing[pacing];
+	const ref4_text = statDescription.moderation[moderation];
 
-  const mid = BASE_VIEWBOX_SIZE / 2 + VIEWBOX_PADDING;
-  const polygonCoordinates = (() => {
-    const addFromCenter = (score: Score) => mid + DISTANCE_INCREMENT * score;
-    const subtractFromCenter = (score: Score) =>
-      mid - DISTANCE_INCREMENT * score;
+	const mid = BASE_VIEWBOX_SIZE / 2 + VIEWBOX_PADDING;
+	const polygonCoordinates = (() => {
+		const addFromCenter = (score: Score) => mid + DISTANCE_INCREMENT * score;
+		const subtractFromCenter = (score: Score) =>
+			mid - DISTANCE_INCREMENT * score;
 
-    const top = {
-      x: mid,
-      y: subtractFromCenter(knowledge),
-    };
-    const right = {
-      x: addFromCenter(speeches),
-      y: mid,
-    };
-    const bottom = {
-      x: mid,
-      y: addFromCenter(pacing),
-    };
-    const left = {
-      x: subtractFromCenter(moderation),
-      y: mid,
-    };
-    const stringify = ({ x, y }: { x: number; y: number }) => `${x} ${y}`;
-    return [
-      stringify(top),
-      stringify(right),
-      stringify(bottom),
-      stringify(left),
-    ];
-  })();
-  const statVectors = `M${polygonCoordinates[3]} L${polygonCoordinates[0]} L${polygonCoordinates[1]} L${polygonCoordinates[2]} L${polygonCoordinates[3]}`;
+		const top = {
+			x: mid,
+			y: subtractFromCenter(knowledge),
+		};
+		const right = {
+			x: addFromCenter(speeches),
+			y: mid,
+		};
+		const bottom = {
+			x: mid,
+			y: addFromCenter(pacing),
+		};
+		const left = {
+			x: subtractFromCenter(moderation),
+			y: mid,
+		};
+		const stringify = ({ x, y }: { x: number; y: number }) => `${x} ${y}`;
+		return [
+			stringify(top),
+			stringify(right),
+			stringify(bottom),
+			stringify(left),
+		];
+	})();
+	const statVectors = `M${polygonCoordinates[3]} L${polygonCoordinates[0]} L${polygonCoordinates[1]} L${polygonCoordinates[2]} L${polygonCoordinates[3]}`;
 
-  const generateInfoCircles = (
-    coordinate: string,
-    persist: boolean,
-    index: number
-  ) => {
-    const CLASSNAME = persist ? "persist" : "animate-on-hover";
-    const [cx, cy, ...rest] = coordinate
-      .trim()
-      .split(" ")
-      .map((coord) => parseFloat(coord));
-    const title = Object.keys(statDescription)[index];
+	const generateInfoCircles = (
+		coordinate: string,
+		persist: boolean,
+		index: number
+	) => {
+		const CLASSNAME = persist ? 'persist' : 'animate-on-hover';
+		const [cx, cy, ...rest] = coordinate
+			.trim()
+			.split(' ')
+			.map((coord) => parseFloat(coord));
+		const title = Object.keys(statDescription)[index];
 
-    const ref = (() => {
-      if (index === 0) return ref1_top;
-      if (index === 1) return ref2_right;
-      if (index === 2) return ref3_bottom;
-      if (index === 3) return ref4_left;
-      return ref1_top;
-    })();
-    return (
-      <>
-        {persist && (
-          <circle cx={cx} cy={cy} r="2" fill="#C4C4C4" className={CLASSNAME} />
-        )}
-        <circle
-          cx={cx}
-          cy={cy}
-          r="7"
-          stroke="#C4C4C4"
-          fill="transparent"
-          className={CLASSNAME}
-          ref={ref}
-        />
-        <circle
-          cx={cx}
-          cy={cy}
-          r="30"
-          fill="transparent"
-          className={CLASSNAME}
-          ref={ref}
-        />
-      </>
-    );
-  };
+		const ref = (() => {
+			if (index === 0) return ref1_top;
+			if (index === 1) return ref2_right;
+			if (index === 2) return ref3_bottom;
+			if (index === 3) return ref4_left;
+			return ref1_top;
+		})();
+		return (
+			<>
+				{persist && (
+					<circle cx={cx} cy={cy} r="2" fill="#C4C4C4" className={CLASSNAME} />
+				)}
+				<circle
+					cx={cx}
+					cy={cy}
+					r="7"
+					stroke="#C4C4C4"
+					fill="transparent"
+					className={CLASSNAME}
+					ref={ref}
+				/>
+				<circle
+					cx={cx}
+					cy={cy}
+					r="30"
+					fill="transparent"
+					className={CLASSNAME}
+					ref={ref}
+				/>
+			</>
+		);
+	};
 
-  return (
-    <StatsContainer>
-      <StatsLabel position="top">
-        <Text align="center" size={isMobile ? "md" : "sm"}>
-          knowledge
-          {/* {` ${knowledge} / 5`} */}
-        </Text>
-      </StatsLabel>
-      <StatsLabel position="right" isMobile={isMobile}>
-        <Text align="center" size={isMobile ? "md" : "sm"}>
-          speeches
-          {/* <br />
+	return (
+		<StatsContainer>
+			<StatsLabel position="top">
+				<Text align="center" size={isMobile ? 'md' : 'sm'}>
+					knowledge
+					{/* {` ${knowledge} / 5`} */}
+				</Text>
+			</StatsLabel>
+			<StatsLabel position="right" isMobile={isMobile}>
+				<Text align="center" size={isMobile ? 'md' : 'sm'}>
+					speeches
+					{/* <br />
           {`${speeches} / 5`} */}
-        </Text>
-      </StatsLabel>
-      <StatsLabel position="bottom">
-        <Text align="center" size={isMobile ? "md" : "sm"}>
-          pacing
-          {/* {` ${pacing} / 5`} */}
-        </Text>
-      </StatsLabel>
-      <StatsLabel position="left" isMobile={isMobile}>
-        <Text align="center" size={isMobile ? "md" : "sm"}>
-          moderation
-          {/* <br /> */}
-          {/* {`${moderation} / 5`} */}
-        </Text>
-      </StatsLabel>
-      <SvgWrapper>
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          width={`${VIEWBOX_DIMENSION}`}
-          height={`${VIEWBOX_DIMENSION}`}
-          viewBox={`0 0 ${VIEWBOX_DIMENSION} ${VIEWBOX_DIMENSION}`}
-          fill="none"
-        >
-          <line x1="16" y1="190.5" x2="366" y2="190.5" stroke="#28317F" />
-          <line x1="190.5" y1="366" x2="190.5" y2="16" stroke="#28317F" />
-          <rect
-            x="15.7071"
-            y="190.494"
-            width="247"
-            height="247"
-            transform="rotate(-45 15.7071 190.494)"
-            stroke="#28317F"
-            fill-opacity="0.5"
-          />
-          <rect
-            x="48.7071"
-            y="190.747"
-            width="200.875"
-            height="200.875"
-            transform="rotate(-45 48.7071 190.747)"
-            stroke="#28317F"
-            fill-opacity="0.5"
-          />
-          <rect
-            x="83.7071"
-            y="190.627"
-            width="149.793"
-            height="149.793"
-            transform="rotate(-45 83.7071 190.627)"
-            stroke="#28317F"
-            fill-opacity="0.5"
-          />
-          <rect
-            x="121.52"
-            y="191.187"
-            width="98.3906"
-            height="98.3906"
-            transform="rotate(-45 121.52 191.187)"
-            stroke="#28317F"
-            fill-opacity="0.5"
-          />
-          <rect
-            x="154.52"
-            y="191.187"
-            width="51.3259"
-            height="51.3259"
-            transform="rotate(-45 154.52 191.187)"
-            stroke="#28317F"
-            fill-opacity="0.5"
-          />
-          <path
-            d={statVectors}
-            fill="url(#paint0_linear_3524_419)"
-            fill-opacity="0.95"
-            className="no-pointers"
-          />
-          {polygonCoordinates.map((coordinate, index) =>
-            generateInfoCircles(coordinate, true, index)
-          )}
-          <defs>
-            <linearGradient
-              id="paint0_linear_3524_419"
-              x1="260.528"
-              y1="0.160081"
-              x2="260.528"
-              y2="449.794"
-              gradientUnits="userSpaceOnUse"
-            >
-              <stop stop-color="#B19967" />
-              <stop offset="1" stop-color="#D4C5A6" />
-            </linearGradient>
-            <clipPath id="clip0_460_429">
-              <rect width="350" height="350" fill="white" />
-            </clipPath>
-          </defs>
-        </svg>
-      </SvgWrapper>
-      <ContextDescription>
-        {ref1_hover && (
-          <Text align="center" size={isMobile ? "md" : "sm"}>
-            {ref1_text}
-          </Text>
-        )}
-        {ref2_hover && (
-          <Text align="center" size={isMobile ? "md" : "sm"}>
-            {ref2_text}
-          </Text>
-        )}
-        {ref3_hover && (
-          <Text align="center" size={isMobile ? "md" : "sm"}>
-            {ref3_text}
-          </Text>
-        )}
-        {ref4_hover && (
-          <Text align="center" size={isMobile ? "md" : "sm"}>
-            {ref4_text}
-          </Text>
-        )}
-      </ContextDescription>
-    </StatsContainer>
-  );
+				</Text>
+			</StatsLabel>
+			<StatsLabel position="bottom">
+				<Text align="center" size={isMobile ? 'md' : 'sm'}>
+					pacing
+					{/* {` ${pacing} / 5`} */}
+				</Text>
+			</StatsLabel>
+			<StatsLabel position="left" isMobile={isMobile}>
+				<Text align="center" size={isMobile ? 'md' : 'sm'}>
+					moderation
+					{/* <br /> */}
+					{/* {`${moderation} / 5`} */}
+				</Text>
+			</StatsLabel>
+			<SvgWrapper>
+				<svg
+					xmlns="http://www.w3.org/2000/svg"
+					width={`${VIEWBOX_DIMENSION}`}
+					height={`${VIEWBOX_DIMENSION}`}
+					viewBox={`0 0 ${VIEWBOX_DIMENSION} ${VIEWBOX_DIMENSION}`}
+					fill="none"
+				>
+					<line x1="16" y1="190.5" x2="366" y2="190.5" stroke="#28317F" />
+					<line x1="190.5" y1="366" x2="190.5" y2="16" stroke="#28317F" />
+					<rect
+						x="15.7071"
+						y="190.494"
+						width="247"
+						height="247"
+						transform="rotate(-45 15.7071 190.494)"
+						stroke="#28317F"
+						fill-opacity="0.5"
+					/>
+					<rect
+						x="48.7071"
+						y="190.747"
+						width="200.875"
+						height="200.875"
+						transform="rotate(-45 48.7071 190.747)"
+						stroke="#28317F"
+						fill-opacity="0.5"
+					/>
+					<rect
+						x="83.7071"
+						y="190.627"
+						width="149.793"
+						height="149.793"
+						transform="rotate(-45 83.7071 190.627)"
+						stroke="#28317F"
+						fill-opacity="0.5"
+					/>
+					<rect
+						x="121.52"
+						y="191.187"
+						width="98.3906"
+						height="98.3906"
+						transform="rotate(-45 121.52 191.187)"
+						stroke="#28317F"
+						fill-opacity="0.5"
+					/>
+					<rect
+						x="154.52"
+						y="191.187"
+						width="51.3259"
+						height="51.3259"
+						transform="rotate(-45 154.52 191.187)"
+						stroke="#28317F"
+						fill-opacity="0.5"
+					/>
+					<path
+						d={statVectors}
+						fill="url(#paint0_linear_3524_419)"
+						fill-opacity="0.95"
+						className="no-pointers"
+					/>
+					{polygonCoordinates.map((coordinate, index) =>
+						generateInfoCircles(coordinate, true, index)
+					)}
+					<defs>
+						<linearGradient
+							id="paint0_linear_3524_419"
+							x1="260.528"
+							y1="0.160081"
+							x2="260.528"
+							y2="449.794"
+							gradientUnits="userSpaceOnUse"
+						>
+							<stop stop-color="#B19967" />
+							<stop offset="1" stop-color="#D4C5A6" />
+						</linearGradient>
+						<clipPath id="clip0_460_429">
+							<rect width="350" height="350" fill="white" />
+						</clipPath>
+					</defs>
+				</svg>
+			</SvgWrapper>
+			<ContextDescription>
+				{!ref1_hover && !ref2_hover && !ref3_hover && !ref4_hover && (
+					<Text align="center" size={isMobile ? 'md' : 'sm'}>
+						hover on a point for details
+					</Text>
+				)}
+				{ref1_hover && (
+					<Text align="center" size={isMobile ? 'md' : 'sm'}>
+						{ref1_text}
+					</Text>
+				)}
+				{ref2_hover && (
+					<Text align="center" size={isMobile ? 'md' : 'sm'}>
+						{ref2_text}
+					</Text>
+				)}
+				{ref3_hover && (
+					<Text align="center" size={isMobile ? 'md' : 'sm'}>
+						{ref3_text}
+					</Text>
+				)}
+				{ref4_hover && (
+					<Text align="center" size={isMobile ? 'md' : 'sm'}>
+						{ref4_text}
+					</Text>
+				)}
+			</ContextDescription>
+		</StatsContainer>
+	);
 };
 
 const StatsContainer = styled(Div)`
-  position: relative;
-  display: flex;
-  justify-content: center;
-  margin: 10px auto 30px;
+	position: relative;
+	display: flex;
+	justify-content: center;
+	margin: 10px auto 30px;
 
-  @media (max-width: ${BREAKPOINTS.sm}) {
-    transform: scale(0.8);
-    max-width: 300px;
-  }
-  @media (min-width: ${BREAKPOINTS.sm}) and (max-width: ${BREAKPOINTS.md}) {
-    transform: scale(0.8);
-  }
-  @media (min-width: ${BREAKPOINTS.md}) {
-    max-width: ${VIEWBOX_DIMENSION + 65}px;
-  }
+	@media (max-width: ${BREAKPOINTS.sm}) {
+		transform: scale(0.8);
+		max-width: 300px;
+	}
+	@media (min-width: ${BREAKPOINTS.sm}) and (max-width: ${BREAKPOINTS.md}) {
+		transform: scale(0.8);
+	}
+	@media (min-width: ${BREAKPOINTS.md}) {
+		max-width: ${VIEWBOX_DIMENSION + 65}px;
+	}
 `;
 
 const StatsLabel = styled.div<{
-  position: "top" | "right" | "bottom" | "left";
-  isMobile?: boolean;
+	position: 'top' | 'right' | 'bottom' | 'left';
+	isMobile?: boolean;
 }>`
-  position: absolute;
+	position: absolute;
 
-  ${({ position, isMobile }) => {
-    if (position === "top")
-      return css`
-        top: -15px;
-      `;
-    if (position === "right")
-      return css`
-        top: 47%;
-        right: ${isMobile ? "-70px" : "-15px"};
-        transform: rotate(90deg); /* Rotates text by 45 degrees */
-      `;
-    if (position === "bottom")
-      return css`
-        /* padding-left: 105px; */
-        bottom: -5px;
-      `;
-    if (position === "left")
-      return css`
-        top: 47%;
-        left: ${isMobile ? "-70px" : "-15px"};
-        transform: rotate(-90deg); /* Rotates text by 45 degrees */
-      `;
-  }}
+	${({ position, isMobile }) => {
+		if (position === 'top')
+			return css`
+				top: -15px;
+			`;
+		if (position === 'right')
+			return css`
+				top: 47%;
+				right: ${isMobile ? '-70px' : '-15px'};
+				transform: rotate(90deg); /* Rotates text by 45 degrees */
+			`;
+		if (position === 'bottom')
+			return css`
+				/* padding-left: 105px; */
+				bottom: -5px;
+			`;
+		if (position === 'left')
+			return css`
+				top: 47%;
+				left: ${isMobile ? '-70px' : '-15px'};
+				transform: rotate(-90deg); /* Rotates text by 45 degrees */
+			`;
+	}}
 `;
 
 const ContextDescription = styled(Div)`
-  position: absolute;
-  height: 0px;
-  margin-top: -20px;
-  bottom: 0;
+	position: absolute;
+	height: 0px;
+	bottom: -20px;
 `;
 
 const SvgWrapper = styled(Div)`
-  .persist {
-    opacity: 1;
-  }
+	.persist {
+		opacity: 1;
+	}
 
-  .no-pointers {
-    pointer-events: none;
-  }
+	.no-pointers {
+		pointer-events: none;
+	}
 
-  .animate-on-hover {
-    opacity: 0.01;
-    transition: transform 0.1s, opacity 0.1s;
-  }
+	.animate-on-hover {
+		opacity: 0.01;
+		transition: transform 0.1s, opacity 0.1s;
+	}
 
-  .animate-on-hover:hover {
-    opacity: 1;
-  }
+	.animate-on-hover:hover {
+		opacity: 1;
+	}
 `;
