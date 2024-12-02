e4d1ad5d97d317c9b07fa7a9db807494ed54af0d
Alexander Kim <alexkim.347@gmail.com>
2024-12-01 21:53:05 -0800

    adding logo and meta tags

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -41,6 +41,19 @@ export default class MyDocument extends Document {
     return (
       <Html>
         <Head>
+          <title>Simulation NATO | Model UN Crisis Conference</title>
+          <meta
+            name="description"
+            content="SimNATO offers a dynamic crisis simulation that mirrors the complexities of real-world NATO operations; In a unique four-way joint crisis model, decisions made in one committee will influence the outcomes of other committees."
+          />
+
+          <meta property="og:image" content="/logo.png" />
+          <meta property="og:image:alt" content="Simulation NATO Logo" />
+
+          <meta name="twitter:card" content="Simulation NATO" />
+          <meta name="twitter:image" content="/logo.png" />
+          <meta name="twitter:image:alt" content="Simulation NATO Logo" />
+
           <link rel="preconnect" href="https://fonts.googleapis.com" />
           <link
             rel="preconnect"
